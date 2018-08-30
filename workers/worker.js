5cac08445472277a2e0b7fdf4244072a8b2c5311
Ulan Degenbaev <ulan@chromium.org>
2018-08-30 11:48:01 +0200

    Polish worker example

--- workers/worker.js
+++ workers/worker.js
@@ -1,25 +1,40 @@
-
-const targetBytes = 50 * 1024 * 1024; // 50MB
+let targetBytes;
+let closeWorker;
 let garbageBytes = 0;
 let garbage = [];
+let timerId = null;
 
 function generateGarbage() {
-  const arrayLength = 1000;
+  if (timerId) {
+    clearTimeout(timerId);
+  }
+  const chunkBytes = 1024 * 1024;
+  const arrayLength = 10000;
   const bytesPerElement = 8;
   const bytesPerArray = arrayLength * bytesPerElement;
-  let array = new Array(arrayLength);
-  for (let j = 0; j < array.length; j++) {
-    array[j] = 1.0 / (generated + j + 1);
+  let generated = 0;
+  while (generated < chunkBytes) {
+    let array = new Array(arrayLength);
+    for (let j = 0; j < array.length; j++) {
+      array[j] = 1.0 / (garbageBytes + j + 1);
+    }
+    garbage.push(array);
+    generated += bytesPerArray;
   }
-  garbage.push(array);
-  garbageBytes += bytesPerArray;
+
+  garbageBytes += generated;
   if (garbageBytes < targetBytes) {
-    setTimeout(generateGarbage, 10);
+    timerId = setTimeout(generateGarbage, 10);
   } else {
     postMessage("done");
+    if (closeWorker) {
+      close();
+    }
   }
 }
 
 onmessage = function(args) {
+  targetBytes = args.data.allocationsPerWorker;
+  closeWorker = args.data.closeWorker;
   setTimeout(generateGarbage, 10);
 }
