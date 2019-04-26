563bb501c1d33f66d13836cb620b26442be636e3
Ulan Degenbaev <ulan@chromium.org>
2019-04-26 17:09:58 +0200

    Change json url in sw.js

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 self.addEventListener('install', event => {
-  fetch('http://0.0.0.0:8000/last.json', {mode: 'no-cors'})
+  fetch('https://drive.google.com/file/d/1ypzN-sygionXCRGzPGdc5PuV0PUKeOkl/view?usp=sharing', {mode: 'no-cors'})
   .then(response => {
     return console.log('yes from sw')
   }).catch(error => {
